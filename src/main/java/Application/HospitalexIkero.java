d48df64ce64e7fe96b341d05cb2842fb90b43eb2
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-05-23 13:35:45 +0200

    GUI para login v.1.0

--- src/main/java/Application/HospitalexIkero.java
+++ src/main/java/Application/HospitalexIkero.java
@@ -1,9 +1,14 @@
 package Application;
 
 
+import GUI.GUILogin;
 
 public class HospitalexIkero {
     public static void main(String[] args) {
+        GUILogin login = new GUILogin();
+
+        login.GUILogin();
+
 
     }
 }
