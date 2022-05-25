c194f500f3721f62f70da37d00552191c1385d0e
Iker Rodriguez <irodrigueziglesias@danielcastelao.org>
2022-05-25 13:21:24 +0200

    Implementado cierta base de usuario

--- src/main/java/Application/HospitalexIkero.java
+++ src/main/java/Application/HospitalexIkero.java
@@ -2,12 +2,14 @@ package Application;
 
 
 import GUI.GUILogin;
+import GUI.GUIUsuario;
 
 public class HospitalexIkero {
     public static void main(String[] args) {
         GUILogin login = new GUILogin();
+        GUIUsuario usuario = new GUIUsuario();
 
-        login.GUILogin();
+       // login.GUILogin();
 
 
     }
---
+++ src/main/java/GUI/GUIUsuario.java
@@ -0,0 +1,61 @@
+package GUI;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+public class GUIUsuario {
+    private JFrame usuario;
+    private JPanel info,bg;
+    private JLabel logo,titulo,background;
+
+    public GUIUsuario() {
+        //Al final de cada componente se añade al bg(Background) label.
+        //Declaraciones
+        usuario = new JFrame("Hospitalex Ikero");
+        bg = new JPanel();
+        info = new JPanel();
+        logo = new JLabel();
+        titulo = new JLabel("Hospitalex Ikero");
+
+        //Frame
+        usuario.setSize(920 ,920);
+        usuario.setResizable(false);
+        usuario.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        usuario.setLayout(null);
+        ImageIcon bgfoto = new ImageIcon("");
+        background.setIcon(bgfoto);
+        background.setBounds(0,0,920,920);
+        bg.setSize(920,920);
+        bg.setOpaque(true);
+        bg.setLayout(null);
+
+        bg.add(background);
+        //Labels
+        titulo.setBounds(250,10,350,60);
+        titulo.setFont(new Font("SansSerif",Font.BOLD,22));
+
+        ImageIcon HI = new ImageIcon("hospitalex ikero.png");
+        logo.setIcon(HI);
+        logo.setBounds(720,20,200,180);
+
+        bg.add(titulo);
+        bg.add(logo);
+
+        //Panel
+        info.setBounds(20,60,700,300);
+        info.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black,3),"Persona"));
+        info.setBackground(new Color(186, 237, 230));
+        bg.add(info);
+
+
+
+        usuario.add(bg);
+        usuario.setVisible(true);
+
+
+
+
+
+    }
+}
