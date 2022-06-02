b61098d2dad7652e4dc042b96b42849ea60bdd5c
Wolke <afernandezmartinez@danielcastelao.org>
2022-06-02 17:17:31 +0200

    Parcheado error al introducir contraseña

--- src/main/java/gestionDatos/BusquedaPaciente.java
+++ src/main/java/gestionDatos/BusquedaPaciente.java
@@ -3,7 +3,8 @@ package gestionDatos;
 import DataBase.DAO.DAOPacientes;
 import DataBase.DVO.Pacientes;
 import GUI.GUIPaciente;
-import logIn.exceptions.NoUserFoundException;
+import gestionDatos.exceptions.NoPatientFound;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,7 +17,7 @@ public class BusquedaPaciente extends GUIPaciente {
         if (pacienteBuscado!=null) new GUIPaciente(pacienteBuscado,true);
 
         else{
-            JOptionPane.showMessageDialog(parent,"Error: No existe el paciente con el DNI introducido. [DNI:"+dniPaciente+"]",
+            JOptionPane.showMessageDialog(parent,new NoPatientFound(dniPaciente).getSimpleMessage(),
                     "Hospitalex Ikero",JOptionPane.WARNING_MESSAGE);
         }
     }
