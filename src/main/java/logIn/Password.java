19ebe50a56d14103008b491520c7d1f95acd1579
AlexFerMar <afernandezmartinez@danielcastelao.org>
2022-06-03 22:45:31 +0200

    Añadido cambio de contraseña

--- src/main/java/logIn/Password.java
+++ src/main/java/logIn/Password.java
@@ -1,24 +1,36 @@
 package logIn;
 
 
+import DataBase.DAO.DAOPersonas;
 import DataBase.DVO.Personas;
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Password {
 
-    public static String hashPassword(String pass){
+    public static String hashPassword(String pass) {
 
         return BCrypt.hashpw(pass, BCrypt.gensalt(13));
     }
 
-    public static boolean isSamePassword(String password,String hash){
+    public static boolean isSamePassword(String password, String hash) {
 
-        return BCrypt.checkpw(password,hash);
+        return BCrypt.checkpw(password, hash);
     }
 
-    public static void changePassword(Personas personas){
+    public static void changePassword(Personas personas, Component parent) {
 
+        String newPassword = JOptionPane.showInputDialog(parent, "Introduce tu nueva contraseña", "Hospitalex Ikero");
 
+        if (isSamePassword(newPassword, personas.getContrasenha())) {
+            String newPassword2 = JOptionPane.showInputDialog(parent, "Introduce de nuevo la contraseña", "Hospitalex Ikero");
+            if (newPassword.equals(newPassword2)) {
+                personas.setContrasenha(newPassword);
+                new DAOPersonas().update(personas);
+            } else JOptionPane.showMessageDialog(parent, "Las contraseñas no coinciden, intentelo de nuevo", "Hospitalex Ikero",JOptionPane.WARNING_MESSAGE);
+        } else JOptionPane.showMessageDialog(parent, "La contraseña no coincide con almacenada,intentelo de nuevo", "Hospitalex Ikero",JOptionPane.WARNING_MESSAGE);
 
     }
 
